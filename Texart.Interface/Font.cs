28821fc7c48066bedde085ff8ee8c6928537cbf4
Pranjal Raihan <pranjal@pranjal-raihan.com>
2019-05-17 12:54:50 -0400

    C# 7 clean up and dictionary.

--- Texart.Interface/Font.cs
+++ Texart.Interface/Font.cs
@@ -20,7 +20,7 @@ namespace Texart.Interface
         /// <exception cref="ArgumentException">If trying to set a value of less than or equal to <code>0</code></exception>
         public int CharacterSpacing
         {
-            get { return this._characterSpacing; }
+            get => this._characterSpacing;
             set
             {
                 if (value <= 0) { throw new ArgumentException($"{nameof(CharacterSpacing)} must be positive"); }
@@ -37,7 +37,7 @@ namespace Texart.Interface
         /// <exception cref="ArgumentException">If trying to set a value of less than or equal to <code>0f</code></exception>
         public float TextSize
         {
-            get { return this._textSize; }
+            get => this._textSize;
             set
             {
                 if (value <= 0f) { throw new ArgumentException($"{nameof(this.TextSize)} must be positive"); }
@@ -77,13 +77,13 @@ namespace Texart.Interface
         /// Gets the default value for character spacing.
         /// </summary>
         /// <see cref="CharacterSpacing"/>
-        public static int DefaultCharacterSpacing { get { return 8; } }
+        public static int DefaultCharacterSpacing => 8;
 
         /// <summary>
         /// Gets the default value for text size.
         /// </summary>
         /// <see cref="TextSize"/>
-        public static float DefaultTextSize { get { return 12f; } }
+        public static float DefaultTextSize => 12f;
 
         /// <summary>
         /// Gets the default value for color.
