53d229733b31ea7f2a19f7a50f8a8d07b69c99e0
Pranjal Raihan <pranjal@pranjal-raihan.com>
2019-05-17 04:34:57 -0400

    Implement a switched reference resolver.

---
+++ Texart.Scripting/SourceReferenceScheme.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace Texart.Scripting
+{
+    public readonly struct SourceReferenceScheme : IComparable<SourceReferenceScheme>, IEquatable<SourceReferenceScheme>
+    {
+        /// <summary>
+        /// All valid schemes must match this regex
+        /// </summary>
+        private static readonly Regex SchemeRegex = new Regex("[a-zA-Z][a-zA-Z\\-0-9]", RegexOptions.Compiled);
+
+        /// <summary>
+        /// The underlying string.
+        /// </summary>
+        public string Scheme { get; }
+        /// <summary>
+        /// The actual prefix of a path that represents this scheme.
+        /// </summary>
+        public string SchemePrefix => $"{Scheme}:";
+
+        /// <summary>
+        /// Constructs a scheme from the given string. It must match <see cref="SchemeRegex"/>.
+        /// </summary>
+        /// <param name="scheme"></param>
+        public SourceReferenceScheme(string scheme)
+        {
+            if (scheme == null || !SchemeRegex.IsMatch(scheme))
+            {
+                throw new ArgumentException($"{nameof(scheme)} is not valid: {scheme}");
+            }
+
+            this.Scheme = scheme;
+        }
+
+        /// <summary>
+        /// Determines whether or not the provided path matches this scheme.
+        /// </summary>
+        /// <param name="path">the path to check against</param>
+        /// <returns>true if match, false otherwise</returns>
+        public bool Matches(string path) => path.StartsWith(SchemePrefix);
+
+        /// <summary>
+        /// Returns the provided path with this scheme removed.
+        /// <see cref="Matches"/> must return <code>true</code> with the provided path.
+        /// </summary>
+        /// <param name="path">the path to remove scheme from</param>
+        /// <returns>the path without this scheme</returns>
+        public string NormalizePath(string path)
+        {
+            Debug.Assert(this.Matches(path));
+            return path.Substring(SchemePrefix.Length);
+        }
+        /// <summary>
+        /// Returns the provided path with this scheme applied.
+        /// </summary>
+        /// <param name="path">the path to add scheme to</param>
+        /// <returns>the path with this scheme</returns>
+        public string Prefix(string path) => $"{SchemePrefix}{path}";
+
+        /// <inheritdoc />
+        public override string ToString() => Scheme;
+
+        /// <inheritdoc />
+        public int CompareTo(SourceReferenceScheme other) =>
+            string.Compare(Scheme, other.Scheme, StringComparison.Ordinal);
+
+        /// <inheritdoc />
+        public bool Equals(SourceReferenceScheme other) => Scheme == other.Scheme;
+
+        /// <inheritdoc />
+        public override bool Equals(object obj) => obj is SourceReferenceScheme scheme && Equals(scheme);
+
+        /// <inheritdoc />
+        public override int GetHashCode() => Scheme.GetHashCode();
+    }
+}
---
+++ Texart.Scripting/SwitchedSourceReferenceResolver.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace Texart.Scripting
+{
+    /// <summary>
+    /// A reference resolver that selects one of multiple resolvers based on a matching <see cref="SourceReferenceScheme"/>.
+    /// In the case where there is no matching scheme, it uses the default resolver as a fallback.
+    /// </summary>
+    public class SwitchedSourceReferenceResolver : SourceReferenceResolver, IEquatable<SwitchedSourceReferenceResolver>
+    {
+        private readonly SourceReferenceResolver _defaultResolver;
+        private readonly IImmutableDictionary<SourceReferenceScheme, SourceReferenceResolver> _resolversByScheme;
+
+        public SwitchedSourceReferenceResolver(SourceReferenceResolver defaultResolver, IImmutableDictionary<string, SourceReferenceResolver> resolversByScheme) :
+            this(defaultResolver, ToSchemeKeyDictionary(resolversByScheme))
+        {
+        }
+
+        public SwitchedSourceReferenceResolver(SourceReferenceResolver defaultResolver, IImmutableDictionary<SourceReferenceScheme, SourceReferenceResolver> resolversByScheme)
+        {
+            this._defaultResolver = defaultResolver ?? throw new ArgumentNullException(nameof(defaultResolver));
+            this._resolversByScheme = resolversByScheme ?? throw new ArgumentNullException(nameof(resolversByScheme));
+        }
+
+        /// <inheritdoc />
+        public override string NormalizePath(string path, string baseFilePath)
+        {
+            var (scheme, resolver) = GetResolverByPath(path);
+            var normalizedPath = resolver.NormalizePath(
+                scheme.HasValue ? scheme.Value.NormalizePath(path) : path,
+                baseFilePath);
+            return scheme.HasValue ? scheme.Value.Prefix(normalizedPath) : normalizedPath;
+        }
+
+        /// <inheritdoc />
+        public override string ResolveReference(string path, string baseFilePath)
+        {
+            var (scheme, resolver) = GetResolverByPath(path);
+            var resolvedReferencePath = resolver.ResolveReference(
+                scheme.HasValue ? scheme.Value.NormalizePath(path) : path,
+                baseFilePath);
+            return scheme.HasValue ? scheme.Value.Prefix(resolvedReferencePath) : resolvedReferencePath;
+        }
+
+        /// <inheritdoc />
+        public override Stream OpenRead(string resolvedPath)
+        {
+            var (scheme, resolver) = GetResolverByPath(resolvedPath);
+            return resolver.OpenRead(scheme.HasValue ? scheme.Value.NormalizePath(resolvedPath) : resolvedPath);
+        }
+
+        private (SourceReferenceScheme?, SourceReferenceResolver) GetResolverByPath(string path) =>
+            _resolversByScheme
+                .Where(kv => kv.Key.Matches(path))
+                .Select(kv => (new SourceReferenceScheme?(kv.Key), kv.Value))
+                .DefaultIfEmpty((null, _defaultResolver))
+                .First();
+
+        /// <inheritdoc />
+        public bool Equals(SwitchedSourceReferenceResolver other) =>
+            _defaultResolver.Equals(other._defaultResolver) && _resolversByScheme.Equals(other._resolversByScheme);
+
+        /// <inheritdoc />
+        public override bool Equals(object other)
+        {
+            switch (other)
+            {
+                case SwitchedSourceReferenceResolver resolver when resolver.GetType() == this.GetType():
+                    return this.Equals(resolver);
+                default:
+                    return false;
+            }
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode() => HashCode.Combine(_defaultResolver, _resolversByScheme);
+
+        private static IImmutableDictionary<SourceReferenceScheme, T> ToSchemeKeyDictionary<T>(IImmutableDictionary<string, T> dictionary) =>
+            dictionary.ToImmutableDictionary(
+                keyValuePair => new SourceReferenceScheme(keyValuePair.Key),
+                keyValuePair => keyValuePair.Value);
+    }
+}
