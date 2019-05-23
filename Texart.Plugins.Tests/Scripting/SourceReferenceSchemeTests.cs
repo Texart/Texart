233a80b4684cb158776132b66a8ef5a0b5f6a1b4
Pranjal Raihan <pranjal@pranjal-raihan.com>
2019-05-23 16:55:21 -0400

    Refactor SourceReferenceSchemeTests.

--- Texart.Plugins.Tests/Scripting/SourceReferenceSchemeTests.cs
+++ Texart.Plugins.Tests/Scripting/SourceReferenceSchemeTests.cs
@@ -4,74 +4,68 @@ using Texart.Plugins.Scripting;
 
 namespace Texart.Plugins.Tests.Scripting
 {
-    internal static class SourceReferenceSchemeTests
+    internal class SourceReferenceSchemeTests
     {
-        private class AllowanceTests
-        {
-            [Test]
-            public void AllowsSimple() => AssertValidScheme("hello");
+        [Test]
+        public void AllowsSimple() => AssertValidScheme("hello");
 
-            [Test]
-            public void AllowsSingleCharacter() => AssertValidScheme("h");
+        [Test]
+        public void AllowsSingleCharacter() => AssertValidScheme("h");
 
-            [Test]
-            public void AllowsTwoCharacters() => AssertValidScheme("he");
+        [Test]
+        public void AllowsTwoCharacters() => AssertValidScheme("he");
 
-            [Test]
-            public void AllowsNumbers() => AssertValidScheme("h0t");
+        [Test]
+        public void AllowsNumbers() => AssertValidScheme("h0t");
 
-            [Test]
-            public void AllowsHyphen() => AssertValidScheme("hello-world");
+        [Test]
+        public void AllowsHyphen() => AssertValidScheme("hello-world");
 
-            private static void AssertValidScheme(string scheme) =>
-                Assert.DoesNotThrow(() => new SourceReferenceScheme(scheme));
+        private static void AssertValidScheme(string scheme) =>
+            Assert.DoesNotThrow(() => new SourceReferenceScheme(scheme));
+
+        [Test]
+        public void RejectsEmpty() => AssertInvalidScheme(string.Empty);
+
+        [Test]
+        public void RejectsWhitespace() {
+            AssertInvalidScheme("hello world");
+            AssertInvalidScheme("hello\tworld");
+            AssertInvalidScheme("\nhello");
+            AssertInvalidScheme("\r\nhello");
+            AssertInvalidScheme("hello\n");
+            AssertInvalidScheme("hello\r\n");
+            AssertInvalidScheme("hello\n\r");
+            AssertInvalidScheme("hello\nworld");
         }
 
-        private class RejectionTests
+        [Test]
+        public void RejectsHyphenAtStart() => AssertInvalidScheme("-hello");
+
+        [Test]
+        public void RejectsOnlyHyphen() => AssertInvalidScheme("-");
+
+        [Test]
+        public void RejectsNumberAtStart() => AssertInvalidScheme("4hello");
+
+        [Test]
+        public void RejectsOnlyNumber() => AssertInvalidScheme("0");
+
+        [Test]
+        public void RejectsUnderscore() => AssertInvalidScheme("hel_lo");
+
+        [Test]
+        public void RejectsColon() => AssertInvalidScheme("he:lo");
+
+        [Test]
+        public void RejectsColonAtEnd() => AssertInvalidScheme("hello:");
+
+        private static void AssertInvalidScheme(string scheme)
         {
-            [Test]
-            public void RejectsEmpty() => AssertInvalidScheme(string.Empty);
-
-            [Test]
-            public void RejectsWhitespace() {
-                AssertInvalidScheme("hello world");
-                AssertInvalidScheme("hello\tworld");
-                AssertInvalidScheme("\nhello");
-                AssertInvalidScheme("\r\nhello");
-                AssertInvalidScheme("hello\n");
-                AssertInvalidScheme("hello\r\n");
-                AssertInvalidScheme("hello\n\r");
-                AssertInvalidScheme("hello\nworld");
-            }
-
-            [Test]
-            public void RejectsHyphenAtStart() => AssertInvalidScheme("-hello");
-
-            [Test]
-            public void RejectsOnlyHyphen() => AssertInvalidScheme("-");
-
-            [Test]
-            public void RejectsNumberAtStart() => AssertInvalidScheme("4hello");
-
-            [Test]
-            public void RejectsOnlyNumber() => AssertInvalidScheme("0");
-
-            [Test]
-            public void RejectsUnderscore() => AssertInvalidScheme("hel_lo");
-
-            [Test]
-            public void RejectsColon() => AssertInvalidScheme("he:lo");
-
-            [Test]
-            public void RejectsColonAtEnd() => AssertInvalidScheme("hello:");
-
-            private static void AssertInvalidScheme(string scheme)
-            {
-                var ex = Assert.Throws<ArgumentException>(() => new SourceReferenceScheme(scheme));
-                Assert.IsTrue(
-                    ex.Message.StartsWith("scheme is not valid"),
-                    "Exception was thrown but not because of invalid scheme");
-            }
+            var ex = Assert.Throws<ArgumentException>(() => new SourceReferenceScheme(scheme));
+            Assert.IsTrue(
+                ex.Message.StartsWith("scheme is not valid"),
+                "Exception was thrown but not because of invalid scheme");
         }
     }
 }
