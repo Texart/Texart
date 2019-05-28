96fe24e8e0073e569d80270a52de7cde6b8d6bba
Pranjal Raihan <pranjal@pranjal-raihan.com>
2019-05-28 04:31:28 -0400

    Bolster RequiredReferenceDirectivesAnalyzer and its tests.

--- Texart.Plugins.Tests/Scripting/Diagnostics/RequiredReferenceDirectivesAnalyzerTests.cs
+++ Texart.Plugins.Tests/Scripting/Diagnostics/RequiredReferenceDirectivesAnalyzerTests.cs
@@ -41,6 +41,160 @@ namespace Texart.Plugins.Tests.Scripting.Diagnostics
         }
 
         [Test]
+        public void AllowsRequiredReferencesLaterInCode()
+        {
+            const string code = @"
+#r ""A.dll""
+#r ""B.dll""
+#r ""C.dll""
+// namespace Foo
+namespace Foo {}
+#r ""A.dll""";
+            VerifyCSharpDiagnostic(code);
+        }
+
+        [Test]
+        public void AllowsOtherReferences()
+        {
+            const string code = @"
+#r ""A.dll""
+#r ""B.dll""
+#r ""C.dll""
+#r ""Stuff.dll""
+namespace Foo {}
+#r ""Stuff.dll""";
+            VerifyCSharpDiagnostic(code);
+        }
+
+        [Test]
+        public void AllowsPrecedingComments()
+        {
+            const string code = @"
+// sample comment
+#r ""A.dll""
+#r ""B.dll""
+#r ""C.dll""
+namespace Foo {}";
+            VerifyCSharpDiagnostic(code);
+        }
+
+        [Test]
+        public void AllowsCommentsAndWhitespaceInBetween()
+        {
+            const string code = @"
+
+// sample comment
+
+#r ""A.dll""
+#r ""B.dll""
+
+// sample comment 2
+
+#r ""C.dll""
+namespace Foo {}";
+            VerifyCSharpDiagnostic(code);
+        }
+
+        [Test]
+        public void AllowsRegionAroundReferences()
+        {
+            const string code = @"
+#region Region
+#r ""A.dll""
+#r ""B.dll""
+#r ""C.dll""
+#endregion";
+            VerifyCSharpDiagnostic(code);
+        }
+
+        [Test]
+        public void AllowsRegionBetweenReferences()
+        {
+            const string code = @"
+#region Region
+#r ""A.dll""
+#r ""B.dll""
+#endregion
+#r ""C.dll""";
+            VerifyCSharpDiagnostic(code);
+        }
+
+        [Test]
+        public void AllowsPragmaBetweenReferences()
+        {
+            const string code = @"
+#r ""A.dll""
+#r ""B.dll""
+#pragma warning disable 414
+#r ""C.dll""";
+            VerifyCSharpDiagnostic(code);
+        }
+
+        [Test]
+        public void RejectsIfDirective()
+        {
+            const string code = @"
+#if DEBUG
+#r ""A.dll""
+#else
+#r ""B.dll""
+#endif
+#r ""C.dll""";
+            var expected = new[]
+            {
+                new DiagnosticResult
+                {
+                    Id = DiagnosticConstants.TexartReferenceDirectiveAnalyzerId,
+                    Message = string.Format(
+                        RequiredReferenceDirectivesAnalyzer.DirectiveNotAllowedFormat, "if"),
+                    Severity = DiagnosticSeverity.Error,
+                    Locations = new[] {new DiagnosticResultLocation(string.Empty, 2, 1)}
+                },
+                new DiagnosticResult
+                {
+                    Id = DiagnosticConstants.TexartReferenceDirectiveAnalyzerId,
+                    Message = string.Format(
+                        RequiredReferenceDirectivesAnalyzer.ScriptMustReferenceFormat, @"""A.dll"", ""B.dll"", ""C.dll"""),
+                    Severity = DiagnosticSeverity.Error,
+                    Locations = new[] {new DiagnosticResultLocation(string.Empty, 2, 1)}
+                },
+                new DiagnosticResult
+                {
+                    Id = DiagnosticConstants.TexartReferenceDirectiveAnalyzerId,
+                    Message = string.Format(
+                        RequiredReferenceDirectivesAnalyzer.DirectiveNotAllowedFormat, "else"),
+                    Severity = DiagnosticSeverity.Error,
+                    Locations = new[] {new DiagnosticResultLocation(string.Empty, 4, 1)}
+                },
+                new DiagnosticResult
+                {
+                    Id = DiagnosticConstants.TexartReferenceDirectiveAnalyzerId,
+                    Message = string.Format(
+                        RequiredReferenceDirectivesAnalyzer.ScriptMustReferenceFormat, @"""B.dll"", ""C.dll"""),
+                    Severity = DiagnosticSeverity.Error,
+                    Locations = new[] {new DiagnosticResultLocation(string.Empty, 4, 1)}
+                },
+                new DiagnosticResult
+                {
+                    Id = DiagnosticConstants.TexartReferenceDirectiveAnalyzerId,
+                    Message = string.Format(
+                        RequiredReferenceDirectivesAnalyzer.DirectiveNotAllowedFormat, "endif"),
+                    Severity = DiagnosticSeverity.Error,
+                    Locations = new[] {new DiagnosticResultLocation(string.Empty, 6, 1)}
+                },
+                new DiagnosticResult
+                {
+                    Id = DiagnosticConstants.TexartReferenceDirectiveAnalyzerId,
+                    Message = string.Format(
+                        RequiredReferenceDirectivesAnalyzer.ScriptMustReferenceFormat, @"""C.dll"""),
+                    Severity = DiagnosticSeverity.Error,
+                    Locations = new[] {new DiagnosticResultLocation(string.Empty, 6, 1)}
+                },
+            };
+            VerifyCSharpDiagnostic(code, expected);
+        }
+
+        [Test]
         public void RejectsDisjointReferences()
         {
             const string code = @"
@@ -52,7 +206,7 @@ namespace Texart.Plugins.Tests.Scripting.Diagnostics
             {
                 Id = DiagnosticConstants.TexartReferenceDirectiveAnalyzerId,
                 Message = string.Format(
-                    RequiredReferenceDirectivesAnalyzer.ScriptMustReferenceFormat, "B.dll"),
+                    RequiredReferenceDirectivesAnalyzer.ScriptMustReferenceFormat, @"""B.dll"""),
                 Severity = DiagnosticSeverity.Error,
                 Locations = new[] { new DiagnosticResultLocation(string.Empty, 4, 4) }
             };
@@ -67,12 +221,24 @@ namespace Texart.Plugins.Tests.Scripting.Diagnostics
 #load ""helper.csx""
 #r ""C.dll""
 #r ""B.dll""";
-            var expected = new DiagnosticResult
+            var expected = new[]
             {
-                Id = DiagnosticConstants.TexartReferenceDirectiveAnalyzerId,
-                Message = RequiredReferenceDirectivesAnalyzer.LoadDirectiveNotAllowedBeforeFormat,
-                Severity = DiagnosticSeverity.Error,
-                Locations = new[] { new DiagnosticResultLocation(string.Empty, 3, 1) }
+                new DiagnosticResult
+                {
+                    Id = DiagnosticConstants.TexartReferenceDirectiveAnalyzerId,
+                    Message = string.Format(
+                        RequiredReferenceDirectivesAnalyzer.DirectiveNotAllowedFormat, "load"),
+                    Severity = DiagnosticSeverity.Error,
+                    Locations = new[] { new DiagnosticResultLocation(string.Empty, 3, 1) }
+                },
+                new DiagnosticResult
+                {
+                    Id = DiagnosticConstants.TexartReferenceDirectiveAnalyzerId,
+                    Message = string.Format(
+                        RequiredReferenceDirectivesAnalyzer.ScriptMustReferenceFormat, @"""B.dll"", ""C.dll"""),
+                    Severity = DiagnosticSeverity.Error,
+                    Locations = new[] { new DiagnosticResultLocation(string.Empty, 3, 1) }
+                },
             };
             VerifyCSharpDiagnostic(code, expected);
         }
@@ -89,7 +255,8 @@ namespace Texart.Plugins.Tests.Scripting.Diagnostics
                 new DiagnosticResult
                 {
                     Id = DiagnosticConstants.TexartReferenceDirectiveAnalyzerId,
-                    Message = RequiredReferenceDirectivesAnalyzer.LoadDirectiveNotAllowedBeforeFormat,
+                    Message = string.Format(
+                        RequiredReferenceDirectivesAnalyzer.DirectiveNotAllowedFormat, "load"),
                     Severity = DiagnosticSeverity.Error,
                     Locations = new[] { new DiagnosticResultLocation(string.Empty, 3, 1) }
                 },
@@ -97,7 +264,15 @@ namespace Texart.Plugins.Tests.Scripting.Diagnostics
                 {
                     Id = DiagnosticConstants.TexartReferenceDirectiveAnalyzerId,
                     Message = string.Format(
-                        RequiredReferenceDirectivesAnalyzer.ScriptMustReferenceFormat, "C.dll"),
+                        RequiredReferenceDirectivesAnalyzer.ScriptMustReferenceFormat, @"""B.dll"", ""C.dll"""),
+                    Severity = DiagnosticSeverity.Error,
+                    Locations = new[] { new DiagnosticResultLocation(string.Empty, 3, 1) }
+                },
+                new DiagnosticResult
+                {
+                    Id = DiagnosticConstants.TexartReferenceDirectiveAnalyzerId,
+                    Message = string.Format(
+                        RequiredReferenceDirectivesAnalyzer.ScriptMustReferenceFormat, @"""C.dll"""),
                     Severity = DiagnosticSeverity.Error,
                     Locations = new[] { new DiagnosticResultLocation(string.Empty, 4, 11) }
                 },
@@ -115,7 +290,7 @@ namespace Texart.Plugins.Tests.Scripting.Diagnostics
             {
                 Id = DiagnosticConstants.TexartReferenceDirectiveAnalyzerId,
                 Message = string.Format(
-                    RequiredReferenceDirectivesAnalyzer.ScriptMustReferenceFormat, "C.dll"),
+                    RequiredReferenceDirectivesAnalyzer.ScriptMustReferenceFormat, @"""C.dll"""),
                 Severity = DiagnosticSeverity.Error,
                 Locations = new[] { new DiagnosticResultLocation(string.Empty, 3, 11) }
             };
@@ -136,7 +311,7 @@ using System;
                 {
                     Id = DiagnosticConstants.TexartReferenceDirectiveAnalyzerId,
                     Message = string.Format(
-                        RequiredReferenceDirectivesAnalyzer.ScriptMustReferenceFormat, "A.dll"),
+                        RequiredReferenceDirectivesAnalyzer.ScriptMustReferenceFormat, @"""A.dll"""),
                     Severity = DiagnosticSeverity.Error,
                     Locations = new[] { new DiagnosticResultLocation(string.Empty, 2, 11) }
                 },
@@ -144,7 +319,7 @@ using System;
                 {
                     Id = DiagnosticConstants.TexartReferenceDirectiveAnalyzerId,
                     Message = string.Format(
-                        RequiredReferenceDirectivesAnalyzer.ScriptMustReferenceFormat, "B.dll"),
+                        RequiredReferenceDirectivesAnalyzer.ScriptMustReferenceFormat, @"""B.dll"""),
                     Severity = DiagnosticSeverity.Error,
                     Locations = new[] { new DiagnosticResultLocation(string.Empty, 2, 11) }
                 },
@@ -152,7 +327,7 @@ using System;
                 {
                     Id = DiagnosticConstants.TexartReferenceDirectiveAnalyzerId,
                     Message = string.Format(
-                        RequiredReferenceDirectivesAnalyzer.ScriptMustReferenceFormat, "C.dll"),
+                        RequiredReferenceDirectivesAnalyzer.ScriptMustReferenceFormat, @"""C.dll"""),
                     Severity = DiagnosticSeverity.Error,
                     Locations = new[] { new DiagnosticResultLocation(string.Empty, 2, 11) }
                 },
--- Texart.Plugins/Scripting/Diagnostics/RequiredReferenceDirectivesAnalyzer.cs
+++ Texart.Plugins/Scripting/Diagnostics/RequiredReferenceDirectivesAnalyzer.cs
@@ -30,25 +30,39 @@ namespace Texart.Plugins.Scripting.Diagnostics
         /// <summary>
         /// See <see cref="DiagnosticDescriptor.Title"/>.
         /// </summary>
-        private const string Title = "Texart scripts must begin with pre-determined reference directives (\"#r\")";
+        private const string Title =
+            "Texart scripts must begin with pre-determined reference directives (\"#r\").";
+
         /// <summary>
         /// See <see cref="DiagnosticDescriptor.MessageFormat"/>.
         /// </summary>
-        internal const string ScriptMustReferenceFormat = "Texart scripts must reference {0} before other code. Add \'#r \"{0}\"' at the top of the file.";
+        internal const string ScriptMustReferenceFormat =
+            @"Texart script must reference one of {0} before other code. Add '#r ""<reference>""' at the top of the file.";
+
         /// <summary>
         /// See <see cref="DiagnosticDescriptor.MessageFormat"/>.
         /// </summary>
-        internal const string LoadDirectiveNotAllowedBeforeFormat = "#load directive is not allowed before all required references";
+        internal const string DirectiveNotAllowedFormat =
+            @"Directive ""#{0}"" is not allowed before all required references.";
+
+        /// <summary>
+        /// See <see cref="DiagnosticDescriptor.MessageFormat"/>.
+        /// </summary>
+        internal const string DirectiveNotRecognizedFormat =
+            @"Directive ""#{0}"" not recognized. Texart script must reference one of {1} before other code.";
+
         /// <summary>
         /// See <see cref="DiagnosticDescriptor.Description"/>.
         /// </summary>
         private static readonly string Description =
             "Texart script must begin with the following directives:\n" +
             string.Join('\n', FormatReferences(RequiredReferences));
+
         /// <summary>
         /// See <see cref="DiagnosticDescriptor.DefaultSeverity"/>.
         /// </summary>
         private const DiagnosticSeverity Severity = DiagnosticSeverity.Error;
+
         /// <summary>
         /// The diagnostic rule description.
         /// </summary>
@@ -97,32 +111,43 @@ namespace Texart.Plugins.Scripting.Diagnostics
                 {
                     continue;
                 }
+                Debug.Assert(triviaSyntax.HasStructure);
+                var directive = (DirectiveTriviaSyntax)triviaSyntax.GetStructure();
 
-                if (triviaSyntax.IsKind(SyntaxKind.LoadDirectiveTrivia))
+                if (SafeDirectives.Any(kind => directive.IsKind(kind)))
+                {
+                    // we can safely ignore certain "safe" directives (e.g. #region)
+                    continue;
+                }
+                if (UnsafeDirectives.Any(kind => directive.IsKind(kind)))
                 {
                     context.ReportDiagnostic(Diagnostic.Create(
-                        Descriptor, triviaSyntax.GetLocation(),
-                        LoadDirectiveNotAllowedBeforeFormat));
+                        Descriptor, directive.GetLocation(),
+                        string.Format(DirectiveNotAllowedFormat, directive.DirectiveNameToken.Text)));
+                    // also include what we expect
+                    context.ReportDiagnostic(Diagnostic.Create(
+                        Descriptor, directive.GetLocation(),
+                        string.Format(ScriptMustReferenceFormat, FormatInlineReferences(remainingReferences))));
                     continue;
                 }
 
-                Debug.Assert(triviaSyntax.HasStructure);
-                var directive = (DirectiveTriviaSyntax)triviaSyntax.GetStructure();
                 if (!directive.IsKind(SyntaxKind.ReferenceDirectiveTrivia))
                 {
                     context.ReportDiagnostic(Diagnostic.Create(
                         Descriptor, directive.GetLocation(),
-                        string.Format(ScriptMustReferenceFormat, remainingReferences.First())));
+                        string.Format(
+                            DirectiveNotRecognizedFormat, directive.DirectiveNameToken.Text, FormatInlineReferences(remainingReferences))));
                     continue;
                 }
 
+                Debug.Assert(directive.IsKind(SyntaxKind.ReferenceDirectiveTrivia));
                 var referenceDirective = (ReferenceDirectiveTriviaSyntax)directive;
                 // Checking _requiredReferences instead allows duplicate references
                 if (!_requiredReferences.Contains(referenceDirective.File.ValueText))
                 {
                     context.ReportDiagnostic(Diagnostic.Create(
                         Descriptor, referenceDirective.File.GetLocation(),
-                        string.Format(ScriptMustReferenceFormat, remainingReferences.First())));
+                        string.Format(ScriptMustReferenceFormat, FormatInlineReferences(remainingReferences))));
                     continue;
                 }
 
@@ -133,9 +158,43 @@ namespace Texart.Plugins.Scripting.Diagnostics
             {
                 context.ReportDiagnostic(Diagnostic.Create(
                     Descriptor, compilationUnit.GetLocation(),
-                    string.Format(ScriptMustReferenceFormat, reference)));
+                    string.Format(ScriptMustReferenceFormat, Quote(reference))));
             }
         }
+
+        /// <summary>
+        /// List of directive trivia syntax that are considered "safe" to appear in between required references.
+        /// These will be ignored in the diagnostics. For a full of all directives, see
+        /// <see href="https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/preprocessor-directives/"/>.
+        /// </summary>
+        /// <seealso cref="UnsafeDirectives"/>
+        private static readonly ImmutableArray<SyntaxKind> SafeDirectives = ImmutableArray.Create(
+            // #if, #elif, #else, #endif - these are NOT safe
+            // SyntaxKind.IfDirectiveTrivia, SyntaxKind.ElifDirectiveTrivia,
+            // SyntaxKind.ElseDirectiveTrivia, SyntaxKind.EndIfDirectiveTrivia,
+            // #define, #undef
+            SyntaxKind.DefineDirectiveTrivia, SyntaxKind.UndefDirectiveTrivia,
+            // #warning, #error
+            SyntaxKind.WarningDirectiveTrivia, SyntaxKind.ErrorDirectiveTrivia,
+            // #line
+            SyntaxKind.LineDirectiveTrivia,
+            // #region, #endregion
+            SyntaxKind.RegionDirectiveTrivia, SyntaxKind.EndRegionDirectiveTrivia,
+            // #pragma warning, #pragma checksum
+            SyntaxKind.PragmaWarningDirectiveTrivia, SyntaxKind.PragmaChecksumDirectiveTrivia);
+
+        /// <summary>
+        /// List of directive trivia syntax that are NOT considered "safe" to appear in between required references.
+        /// These will cause diagnostic errors. For a full of all directives, see
+        /// <see href="https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/preprocessor-directives/"/>.
+        /// </summary>
+        /// /// <seealso cref="SafeDirectives"/>
+        private static readonly ImmutableArray<SyntaxKind> UnsafeDirectives = ImmutableArray.Create(
+            // #load - executes arbitrary code, so NOT safe
+            SyntaxKind.LoadDirectiveTrivia,
+            // #if, #elif, #else, #endif - conditional compilation makes them NOT safe
+            SyntaxKind.IfDirectiveTrivia, SyntaxKind.ElifDirectiveTrivia,
+            SyntaxKind.ElseDirectiveTrivia, SyntaxKind.EndIfDirectiveTrivia);
 
         /// <summary>
         /// Determines if <paramref name="syntaxTree"/> is from a <c>.tx.csx</c> file.
@@ -152,20 +211,36 @@ namespace Texart.Plugins.Scripting.Diagnostics
         }
 
         /// <summary>
-        /// Format a reference as a C# directive.
+        /// Formats a reference as a C# directive.
         /// </summary>
         /// <param name="reference">The reference file name.</param>
         /// <returns>The formatted directive code.</returns>
         private static string FormatReference(string reference) => $"#r \"{reference}\"";
 
         /// <summary>
-        /// Format multiple references as a bullet list of C# directives.
+        /// Formats multiple references as a bullet list of C# directives.
         /// </summary>
         /// <param name="references">The reference file names.</param>
         /// <returns>The formatted directive code.</returns>
         /// <seealso cref="FormatReference"/>
         private static IEnumerable<string> FormatReferences(IEnumerable<string> references) =>
             references.Select(reference => $"* {FormatReference(reference)}");
+
+        /// <summary>
+        /// Formats multiple references as a comma-delimited inline list of assembly names.
+        /// </summary>
+        /// <param name="references">The reference file names.</param>
+        /// <returns>The formatted directive code.</returns>
+        /// <seealso cref="Quote"/>
+        private static string FormatInlineReferences(IEnumerable<string> references) =>
+            string.Join(", ", references.Select(Quote));
+
+        /// <summary>
+        /// Wraps <paramref name="s"/> in quotes, <c>"</c>.
+        /// </summary>
+        /// <param name="s">The string to wrap.</param>
+        /// <returns><paramref name="s"/> wrapped in quotes, <c>"</c>.</returns>
+        private static string Quote(string s) => $"\"{s}\"";
 
         /// <summary>
         /// See <see cref="RequiredReferences"/>. This is here mostly for unit tests.
