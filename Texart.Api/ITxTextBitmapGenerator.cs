df979fdebee708446016a815db5bc310405bab20
Pranjal Raihan <pranjal@pranjal-raihan.com>
2019-06-26 05:11:31 -0400

    Implement BestSamplingFactor.

--- Texart.Api/ITxTextBitmapGenerator.cs
+++ Texart.Api/ITxTextBitmapGenerator.cs
@@ -11,26 +11,6 @@ namespace Texart.Api
     /// <seealso cref="ITxTextBitmapRenderer"/>
     public interface ITxTextBitmapGenerator
     {
-        // TODO: Move the docs for PixelSamplingRatio to the argument extraction API
-//        /// <summary>
-//        /// Gets the ratio of source resolution to generated resolution. That is, one
-//        /// pixel in the generated text will come from sampling <c>Math.Pow(PixelSamplingRatio, 2)</c>
-//        /// pixels.
-//        /// For example, if <see cref="PixelSamplingRatio"/> is <c>2</c>, then <c>4</c>
-//        /// pixels from the image will be used to generate <c>1</c> character.
-//        /// Consequently, a value of <c>1</c> is loss-less.
-//        /// Note that implementations are only required to support ratios that perfectly divide both the width
-//        /// and height of the provided bitmap.
-//        /// For the sake of maintaining aspect ratio, implementations must "chunk" images evenly on both X and
-//        /// Y axes (using this many pixels).
-//        /// This creates the unfortunately scenario in the case that the width and height are distinct prime
-//        /// numbers (where the only valid sampling ratio is <c>1</c>). In that case, the image should be
-//        /// resized or cropped before text generation.
-//        /// </summary>
-//        /// <see cref="GenerateAsync"/>
-//        /// <see cref="TxBitmap.GetPerfectPixelRatios(SkiaSharp.SKBitmap)"/>
-//        int PixelSamplingRatio { get; }
-
         /// <summary>
         /// Generates one or more <see cref="ITxTextBitmap"/>s from <paramref name="bitmaps"/>.
         /// </summary>
